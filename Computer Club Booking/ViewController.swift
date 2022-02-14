467b560c2d4c1277d3e6505944d54605bcb0476a
OkonovUrmat <okonov.okonov@gmail.com>
2022-02-14 14:22:45 +0600

    Init commit again

---
+++ Computer Club Booking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Computer Club Booking
+//
+//  Created by Urmat on 14/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
